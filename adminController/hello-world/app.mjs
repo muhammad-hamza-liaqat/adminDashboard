e8f9e569dc5e0261c1e20512cadca2be629830e8
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-04-01 09:54:24 +0500

    getUserChainStats -> api into lambda

--- adminController/hello-world/app.mjs
+++ adminController/hello-world/app.mjs
@@ -40,22 +40,22 @@ export const lambdaHandler = async (event) => {
 
 const getAllUser = async (queryParams) => {
   try {
-    const client = await DBConn(); 
-    const db = client.db('10D');
-    const usersCollection = db.collection('users');
+    const client = await DBConn();
+    const db = client.db("10D");
+    const usersCollection = db.collection("users");
     const totalUsersCount = await usersCollection.countDocuments();
 
     const page = Number(queryParams.page) || 1;
     const limit = Number(queryParams.limit) || 10;
     const skip = (page - 1) * limit;
 
-
-    const users = await usersCollection.find({})
+    const users = await usersCollection
+      .find({})
       .skip(skip)
       .limit(limit)
       .toArray();
 
-    const userArr = users.map(user => ({
+    const userArr = users.map((user) => ({
       userId: user._id,
       userName: user.userName,
       totalNodes: user.totalNode,
@@ -63,22 +63,63 @@ const getAllUser = async (queryParams) => {
       userBalance: user.userWallet ? user.userWallet.userBalance : 0,
     }));
 
+    await client.close();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Success",
+        users: userArr,
+        totalUsers: totalUsersCount,
+      }),
+    };
+  } catch (error) {
+    console.error("An error occurred:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "Something Went Wrong",
+        error: error.message,
+      }),
+    };
+  }
+};
+
+const getUserChainStats = async (queryParams) => {
+  try {
+    const client = await DBConn();
+    const db = client.db("10D");
+    const totalUsers = await db.collection("users").countDocuments({});
+    let totalChainInvestment = 0;
+    const chains = await db.collection("chains").find({}).toArray();
+
+    for (const chain of chains) {
+      const collectionName = `treeNodes${chain.name}`;
+      const rootNode = await db
+        .collection(collectionName)
+        .findOne({ _id: chain?.rootNode }, { projection: { totalMembers: 1 } });
+      const chainInvestment = rootNode.totalMembers * chain.seedAmount;
+      totalChainInvestment += chainInvestment;
+    }
 
     await client.close();
 
     return {
       statusCode: 200,
       body: JSON.stringify({
-        message: 'Success',
-        users: userArr,
-        totalUsers: totalUsersCount
-      })
+        message: "Success",
+        totalUsers,
+        totalChainInvestment,
+      }),
     };
   } catch (error) {
-    console.error('An error occurred:', error);
+    console.log("an error has occured", error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ message: 'Something Went Wrong', error: error.message })
+      body: JSON.stringify({
+        message: "something went wrong!, try again later",
+        error: error.message,
+      }),
     };
   }
 };
